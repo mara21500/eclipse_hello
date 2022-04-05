327fb50310c1943fa0b5fef27c9e7f5bfe9ddf5a
hclee <gnscjf33@gmail.com>
2022-04-05 10:05:25 +0900

    added 'your world'

--- src/helloworld2/hello.java
+++ src/helloworld2/hello.java
@@ -1,5 +1,11 @@
 package helloworld2;
 
 public class hello {
+		public static void main(String[] args) {
+		System.out.print("Hello world");
+		System.out.print("Hello his world");
+		
+		}
 
-}
+	}
+
