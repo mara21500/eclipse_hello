c455d922f331a02ae585f5bc2e3812eb78db6335
hclee <gnscjf33@gmail.com>
2022-04-07 11:13:38 +0900

    hellow world

--- src/helloworld2/hello.java
+++ src/helloworld2/hello.java
@@ -4,7 +4,7 @@ public class hello {
 		public static void main(String[] args) {
 		System.out.print("Hello world");
 		System.out.print("Hello his world");
-		
+		System.out.print("Hello your world");
 		}
 
 	}
